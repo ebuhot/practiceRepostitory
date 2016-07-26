ed20c2158f29f222d536dc9806830aea584371fa
DGN1 <dgn1@dgn1s-macbook-air.local>
2016-07-26 14:10:21 -0500

    Initial Commit

---
+++ PracticeGitHub2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PracticeGitHub2
+//
+//  Created by DGN1 on 7/26/16.
+//  Copyright © 2016 DGN1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
